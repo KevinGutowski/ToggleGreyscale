874ff4dc1cf10522906a569a3d6810d27e8505e1
Kevin Gutowski <kgutowski@berkeley.edu>
2020-03-05 02:23:24 -0800

    Initial commit

---
+++ src/my-command.js
@@ -0,0 +1,73 @@
+import sketch from 'sketch'
+// documentation: https://developer.sketchapp.com/reference/api/
+let isRemoving
+
+export default function() {
+    let sketch = require('sketch')
+    let UI = sketch.UI
+    let ShapePath = sketch.ShapePath
+    let Rectangle = sketch.Rectangle
+    let Style = sketch.Style
+
+    let doc = sketch.getSelectedDocument()
+    let selection = doc.selectedLayers.layers
+
+    if (!selection.length) {
+        console.log('triggered')
+        UI.message('Please select a layer')
+        return
+    }
+
+    let correctSelection = selection.find(layer => (layer.type == 'Artboard' || layer.getParentArtboard()))
+
+    if (!correctSelection) {
+        UI.message('Please select artboards or a layers within an artboard')
+        return
+    }
+
+    if (correctSelection.layers.find(layer => layer.name == "Greyscale Layer")) {
+        isRemoving = true
+    } else {
+        isRemoving = false
+    }
+
+    for (let i = 0; i < selection.length; i++) {
+        let parentArtboard
+        let frame
+        let layer = selection[i]
+
+        if (layer.type == 'Artboard') {
+            parentArtboard = layer
+        } else if (layer.getParentArtboard()) {
+            parentArtboard = layer.getParentArtboard()
+        }
+
+        let greyscaleLayer = parentArtboard.layers.find(layer => layer.name == "Greyscale Layer")
+
+        if (isRemoving) {
+            removeGreyscaleLayer(greyscaleLayer)
+            continue
+        }
+
+        frame = new Rectangle(0, 0, parentArtboard.frame.width, parentArtboard.frame.height)
+
+        if (!greyscaleLayer) {
+            let overlay = new ShapePath({
+              parent: parentArtboard,
+              frame: frame,
+              name: "Greyscale Layer",
+              style: { fills: ['#FFFFFF'], borders: [], blendingMode: Style.BlendingMode.Color },
+              locked: true
+            })
+        }
+    }
+}
+
+function removeGreyscaleLayer(layer) {
+  if (layer) {
+    layer.remove()
+    return true
+  } else {
+    return false
+  }
+}
